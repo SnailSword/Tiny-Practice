fda45b2d2e96624db9caba48bc120b9560a6b601
SnailSword <saniac@163.com>
2016-08-02 14:50:08 +0800

    css

---
+++ slideshow/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Saniac on 2016/8/1.
+ */
+requirejs.config({
+    path:{
+        jquery:'jquery-1.7.2.min'
+    }
+});
+requirejs(['jquery','slideshow'],function ($,sS) {
+    
+});
---
+++ slideshow/js/slideshow.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Saniac on 2016/8/1.
+ */
+define(['jquery'],function($){
+
+    return{
+        slideShow : slideShow
+    }
+});
